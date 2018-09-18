bb37ed9ac28752066f9111cb9fc707b5a2704601
Abdullah Basit <abdullah.basit@hotmail.com>
2018-09-18 15:17:15 +0500

    Fixed system recordings

--- app/SystemRecording.php
+++ app/SystemRecording.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class SystemRecording extends Model
 {
+	protected $fillable = ['name', 'path'];
+
     public function announcement()
     {
     	return $this->hasOne('App\Announcement');
