c71e8c3652f15b72836436588de58e16d2798390
Abdullah Basit <abdullah.basit@hotmail.com>
2019-01-16 16:48:31 +0500

    Added API logging command with exception handling

--- app/Console/Commands/ApiPost.php
+++ app/Console/Commands/ApiPost.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
 use Illuminate\Console\Command;
 use PAMI\Client\Impl\ClientImpl;
 use PAMI\Message\Event\AgentConnectEvent;
@@ -42,18 +43,24 @@ class ApiPost extends Command
     public function handle()
     {
         $manager = new ClientImpl($this->getAMIOptions());
-        $manager->registerEventListener(function ($event) {
-            $agent = explode("/", $event->getKey('Interface'))[1];
-            $phone = $event->getKey('CallerIDNum');
-            $client = new Client();
-            $response = $client->request('POST', 'http://192.168.0.79/Leop/api/UserProfiling/Add', [
-                'UserId' => $agent,
-                'PhoneNo' => $phone
-            ]);
-            $this->info($response->getBody());
-        }, function ($event) {
-            return $event instanceof AgentConnectEvent;
-        });
+        try {
+            $manager->registerEventListener(function ($event) {
+                $agent = explode("/", $event->getKey('Interface'))[1];
+                $phone = $event->getKey('CallerIDNum');
+                $client = new Client([
+                    'timeout' => 10
+                ]);
+                $response = $client->request('POST', 'http://192.168.0.79/Leop/api/UserProfiling/Add', [
+                    'UserId' => $agent,
+                    'PhoneNo' => $phone
+                ]);
+                $this->info($response->getBody());
+            }, function ($event) {
+                return $event instanceof AgentConnectEvent;
+            });
+        } catch (RequestException $e) {
+            $this->info($e->getResponse());
+        }
         $manager->open();
         while (true) {
             usleep(1000);
