67cad20de574cf100dd4249f6b05833e7c00bdcc
Abdullah Basit <abdullah.basit@hotmail.com>
2019-01-16 15:23:05 +0500

    Added API logging command

---
+++ app/Console/Commands/ApiPost.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Console\Commands;
+
+use GuzzleHttp\Client;
+use Illuminate\Console\Command;
+use PAMI\Client\Impl\ClientImpl;
+use PAMI\Message\Event\AgentConnectEvent;
+use Setting;
+
+class ApiPost extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'api:start';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command to log incoming call data via API';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $manager = new ClientImpl($this->getAMIOptions());
+        $manager->registerEventListener(function ($event) {
+            $agent = explode("/", $event->getKey('Interface'))[1];
+            $phone = $event->getKey('CallerIDNum');
+            $client = new Client();
+            $response = $client->request('POST', 'http://192.168.0.79/Leop/api/UserProfiling/Add', [
+                'UserId' => $agent,
+                'PhoneNo' => $phone
+            ]);
+            $this->info($response->getBody());
+        }, function ($event) {
+            return $event instanceof AgentConnectEvent;
+        });
+        $manager->open();
+        while (true) {
+            usleep(1000);
+            $manager->process();
+        }
+    }
+
+    private function getAMIOptions()
+    {
+        return $options = [
+            // 'host' => '192.168.1.109',
+            'host' => Setting::get('host'),
+            'port' => (int)Setting::get('port'),
+            'username' => Setting::get('username'),
+            'secret' => Setting::get('secret'),
+            'connect_timeout' => (int)Setting::get('connect_timeout'),
+            'read_timeout' => (int)Setting::get('read_timeout'),
+            'scheme' => 'tcp://' // try tls://
+        ];
+    }
+}
